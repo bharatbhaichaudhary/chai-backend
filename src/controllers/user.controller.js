a6166927d29f3a6aaffdeb386858791678e7e64e
bharatbhaichaudhary <bharatch6353@gmail.com>
2024-10-23 09:12:01 +0530

    add channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -318,6 +318,79 @@ const updateUSerCoverImage = asyncHandler(async (req, res) => {
   )
 });
 
+const getUserChannelProfile = asyncHandler(async (req,res)=>{
+
+  const {username} = req.params
+
+  if(!username?.trim){
+    throw new ApiError(400, 'username is missing')
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match:{username: username.toLowerCase()}
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+      }
+    },
+    {
+      $lookup:{
+        from: "subscriptions",
+        localField:"_id",
+        foreignField:"subcripber",
+        as:"subscribedTo"
+      }
+    },
+    {
+      $addFields:{
+        subscribersCount:{
+          $size:"$subcripber"
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo"
+      },
+      isSubscribed:{
+        $cond:{
+          if: {$in: [req.user?._id, "$subcripber.subcripber"]},
+          then: true,
+          else: false
+        }
+      }
+      }
+    },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+
+    }
+    }
+  ])
+
+  if (!channel?.length) {
+    throw new ApiError(404, "channel does not exists")
+}
+
+return res
+.status(200)
+.json(
+    new ApiResponse(200, channel[0], "User channel fetched successfully")
+)
+
+  console.log("channel", channel);
+  
+})
 export {
   registerUser,
   loginUser,
@@ -327,5 +400,6 @@ export {
   changeCurrentPassword,
   updateAccountDetails,
   updateUSerAvatar,
-  updateUSerCoverImage
+  updateUSerCoverImage,
+  getUserChannelProfile
 };
